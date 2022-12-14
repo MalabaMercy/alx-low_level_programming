10957e5db89bd5f0987db392aa574f795443d8e5
MalabaMercy <malabamercy622@gmail.com>
2022-12-13 20:24:08 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
+ * main - print a c program using printf
  *
- * *main - Entry point
- *
- * *Return: Always 0 (Success)
- *
- * */
+ *Return: 0
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
-
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
