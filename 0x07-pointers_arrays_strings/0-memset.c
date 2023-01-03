943ac9507fb34b462743332a113e47a0f5a2fc80
MalabaMercy <malabamercy622@gmail.com>
2023-01-03 12:26:23 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+/**
+ *_memset -  function that fills memory with a constant byte
+ *@s: input pointer to char type
+ *@b: input variable of char type
+ *@n: unsigned int variable
+ * Return: A pointer to the filled memory area @s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j;
+
+	for (j = 0; j < size; j++)
+		s[j] = b;
+	return (s);
+}
